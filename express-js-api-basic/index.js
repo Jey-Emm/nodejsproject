afca5a2b4f4d11d5d5bc31e277b0feae24d9a266
gitexplorer <gitexplorer@gmail.com>
2025-09-24 00:11:42 +0800

    Fixed removing all files

--- express-js-api-basic/index.js
+++ express-js-api-basic/index.js
@@ -11,14 +11,6 @@ const app = express();
 //use default json parser
 app.use(express.json());
 
-const temp_port = process.env.API_PORT || 3000;
-//initialize port for the server to listen
-app.listen(temp_port, () => {
-    console.log(`Server is running in port ${temp_port}`);
-    console.log(`ENCRYPTION_KEY: ${process.env.ENCRYPTION_KEY} , ENCRYPTION_IV: ${process.env.ENCRYPTION_IV} , ALGO: ${process.env.ALGORITHM}`);
-});
-
-
 //authenticate route consists of "/" route, a middleware (CheckCredentials)
 app.post("/authenticate", CheckCredentials, async (req,res) => {
 
@@ -126,4 +118,9 @@ app.get("/GetStudentInfo/:id", async (req, res) => {
 })
 
 
-//DBConnection();
+const temp_port = process.env.API_PORT;
+//initialize port for the server to listen
+app.listen(temp_port, () => {
+    console.log(`Server is running in port ${temp_port}`);
+    console.log(`ENCRYPTION_KEY: ${process.env.ENCRYPTION_KEY} , ENCRYPTION_IV: ${process.env.ENCRYPTION_IV} , ALGO: ${process.env.ALGORITHM}`);
+});
