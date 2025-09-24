87d354bdf1d271f828773b96843e8efc2019f3c7
gitexplorer <gitexplorer@gmail.com>
2025-09-24 09:38:26 +0800

    removed debugging logs

--- express-js-api-basic/index.js
+++ express-js-api-basic/index.js
@@ -19,7 +19,7 @@ const temp_port = process.env.API_PORT;
 //initialize port for the server to listen
 app.listen(temp_port, () => {
     console.log(`Server is running in port ${temp_port}`);
-    console.log(`ENCRYPTION_KEY: ${process.env.ENCRYPTION_KEY} , ENCRYPTION_IV: ${process.env.ENCRYPTION_IV} , ALGO: ${process.env.ALGORITHM}`);
+    //console.log(`ENCRYPTION_KEY: ${process.env.ENCRYPTION_KEY} , ENCRYPTION_IV: ${process.env.ENCRYPTION_IV} , ALGO: ${process.env.ALGORITHM}`);
 });
 
 //authenticate route consists of "/" route, a middleware (CheckCredentials)
